21b26f37b419039a3a3fc54fd4da89c61cd80b4d
Ivy Patton <lilaivy@gmail.com>
2017-06-08 12:21:56 -0700

    added GreetingContainer(redux wrapper) to app.js

--- baby-redux/src/App.js
+++ baby-redux/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GreetingContainer from'./containers/Container';
 
 class App extends Component {
     render() {
@@ -10,9 +11,7 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo" />
                     <h2>WELCOME TO BABY REDUX LAB</h2>
                 </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+             <GreetingContainer/>
             </div>
         );
     }
