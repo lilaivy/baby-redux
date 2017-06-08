a84ff6575cae66864b336daec9cdcffadc023ea6
Ivy Patton <lilaivy@gmail.com>
2017-06-08 11:59:21 -0700

    built Greeting component

---
+++ baby-redux/src/components/Greeting.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Greeting(props) {
+
+    const { name, greeting, changeGreeting, changeName } = props;
+
+    return (
+        <div>
+            <h1>{greeting}{name}</h1>
+            <form>
+                <input type="text" value={greeting}
+                    onChange={event => {changeGreeting(event.target.value);
+                    }} />
+                    <input type="text" value={name}
+                    onChange={event => {changeName(event.target.value);
+                    }}/>
+            </form>
+        </div>
+    );
+
+
+
+}
